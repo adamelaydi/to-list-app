867c1ad56b79c4bb47eb3268111494052052caf8
adam elaydi <adamebrahim492006@gmail.com>
2026-06-14 03:24:05 +0300

    todo app with react js

--- src/components/AddForm.jsx
+++ src/components/AddForm.jsx
@@ -4,54 +4,7 @@ import { useToast } from "../context/toastContext"
 import { useReducer ,useEffect} from "react"
 import taskWindowReducer from "../reducers/taskWindowReducer"
 import '../styling/addFormStyling.css'
-//#region styling
-const formStyle={
-    backgroundColor:"rgba(10, 178, 234, 0.58)",
-    display:"flex",
-    justifyContent:"space-around",
-    alignItems:"center",
-    flexDirection:"column",
-    flexWrap:"wrap",
-    width:"80%",
-    maxWidth:"400px",
-    height:"400px",
-    borderRadius:"25px",
-    padding:"0 25px 50px ",
-    position:"absolute",
-    top:"50%",
-    transform:"translateY(-50%) translateX(-50%)",
-    left:"50%",
-    
 
-}
-const InputStyling={
-    width:"80%",
-    height:"35px",
-    paddingLeft:"7px",
-    border:"none",
-    borderRadius:"5px"
-}
-const ButtonStyling={
-                        width:"70px",
-                        height:"32px",
-                        border:"none",
-                        borderRadius:"5px",
-                        fontSize:"15px",
-                        fontWeight:"700",
-                        textTransform:"capitalize",
-                        color:"#777"
-                    }
-const headingStyle={
-                        fontSize:"35px",
-                        fontWeight:"800",
-                        color:"white",
-                    }
-const buttonStyling={
-                        width:"80%",
-                        display:"flex",
-                        justifyContent:"space-around"
-                    }
-//#endregion
 export default function AddForm(){
     const [Taskdata,setTaskData]=useState({title:"",content:""})
     let intialState=localStorage.getItem("tasks")?JSON.parse(localStorage.getItem("tasks")):[]
@@ -63,11 +16,11 @@ export default function AddForm(){
     return(
         <>  
             <div className="popUp">
-                <div style={formStyle}>
-                    <h1 style={headingStyle}>Adding task</h1>
-                    <input style={InputStyling} type="text" onChange={(e)=>{setTaskData({...Taskdata,title:e.target.value});}} value={Taskdata.title} placeholder="Task Title ..."/>
-                    <input style={InputStyling} type="text" onChange={(e)=>{setTaskData({...Taskdata,content:e.target.value});}} value={Taskdata.content} placeholder="Task Describtion ..."/>
-                    <div style={buttonStyling}>
+                <div className="form1">
+                    <h1 >Adding task</h1>
+                    <input name="title" type="text" onChange={(e)=>{setTaskData({...Taskdata,title:e.target.value});}} value={Taskdata.title} placeholder="Task Title ..."/>
+                    <input name="content" type="text" onChange={(e)=>{setTaskData({...Taskdata,content:e.target.value});}} value={Taskdata.content} placeholder="Task Describtion ..."/>
+                    <div>
                     <Link to={-1}>
                         <button onClick={()=>{
                             if(Taskdata.title==""){
@@ -81,10 +34,10 @@ export default function AddForm(){
                                 showHideTask("TASK ADDED !")
                             }
                                 
-                        }} style={ButtonStyling}>add</button>
+                        }}>add</button>
                     </Link>
                     <Link to={-1}>
-                        <button style={ButtonStyling}>back</button>
+                        <button>back</button>
                     </Link>
                     </div>
                 </div>
